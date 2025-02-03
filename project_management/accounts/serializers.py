38521657696285ff180b3f2dacd2377d1d790888
BabaeiZ <z.b.p.3138@gmail.com>
2025-02-03 12:23:23 +0330

    add register api

---
+++ project_management/accounts/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+
+
+class UserRegisterSerializer(ModelSerializer):
+    """
+    Serializer for user registration.
+    """
--- project_management/accounts/views.py
+++ project_management/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserRegisterSerializer
+
+
+class RegisterAPIView(APIView):
+    """
+    This endpoint allows clients to register a new user by providing an email, and password.
+    """
+    
+    permission_classes = [AllowAny]
+    serializer_class = UserRegisterSerializer
+    
+    def post(self, request, *args, **kwargs):
+        return Response({"message": "The User Registered Successfully."}, status=status.HTTP_201_CREATED)
